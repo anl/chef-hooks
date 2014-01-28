916c60d4a606a6600723b71239d9d5f16b981506
Andrew Leonard <andy@hurricane-ridge.com>
2014-01-28 06:12:54 -0800

    Add basic script to automate deploying pre-commit hook.

---
+++ deploy-pre-commit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+while getopts 'u:' flag; do
+    case flag in
+	u) url=$OPTARG ;;
+    esac
+done
+
+$hook_url=${url:-'https://raw.github.com/anl/chef-hooks/master/pre-commit'}
+
+# Get top level of repo, and set path to pre-commit hook:
+$top_level=$(git rev-parse --show-toplevel)
+$pc_hook=${top_level}/.git/hooks/pre-commit
+
+rm -f $pc_hook
+curl -o - $hook_url > $pc_hook
+chmod 755 $pc_hook
+
